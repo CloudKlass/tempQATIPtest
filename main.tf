d0d5613498c161c7bf7b0977206b894000fd41b7
CloudKlass <104594551+CloudKlass@users.noreply.github.com>
2026-03-25 11:20:47 +0000

    Update storage account name and resource location

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "jenkinsstatepault"
+    storage_account_name = "jenkins-state-118"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
@@ -23,5 +23,5 @@ terraform {
 
 resource "azurerm_resource_group" "example" {
   name     = "RG2"
-  location = "westeurope"
+  location = "us-west-2"
 }
